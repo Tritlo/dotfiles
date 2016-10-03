e07292c1ac444216b25e52220a1a6749e3dc2b97
Matthías Páll Gissurarson <mpg@mpg.is>
2016-10-03 15:26:35 +0200

    use chips instead of omf for fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,4 @@
+# chips
+if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
+
+source ~/.config/fish/gnupg.fish
--- fish/.config/omf/init.fish
+++
@@ -1,9 +0,0 @@
-set -x EDITOR vim
-set PATH ~/.local/bin ~/.pyenv/shims $PATH
-
-eval (direnv hook fish)
-fish_vi_mode
-
-set CURDIR (dirname (status --current-filename ))
-source $CURDIR/gnupg.fish
-source $CURDIR/sudo.fish
